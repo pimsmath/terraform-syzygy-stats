53c1da54b1d8c8e82aab555b4a4880409fcaf249
Ian Allison <iana@pims.math.ca>
2019-08-14 13:32:06 -0700

    Migrating to v012 syntax
    Signed-off-by: Ian Allison <iana@pims.math.ca>

--- main.tf
+++ main.tf
@@ -3,26 +3,34 @@ terraform {
   backend "s3" {}
 }
 
+provider "ansible" {
+  version = "~>1.0"
+}
+
+provider "openstack" {
+  version = "~>1.0"
+}
+
 resource "openstack_networking_floatingip_v2" "fip" {
-  pool = "${var.floatingip_pool}"
+  pool = var.floatingip_pool
 }
 
 resource "openstack_compute_floatingip_associate_v2" "fip" {
-  instance_id = "${openstack_compute_instance_v2.hub.id}"
-  floating_ip = "${openstack_networking_floatingip_v2.fip.address}"
+  instance_id = openstack_compute_instance_v2.hub.id
+  floating_ip = openstack_networking_floatingip_v2.fip.address
 }
 
 resource "openstack_compute_instance_v2" "hub" {
-  name            = "${var.environment_name}"
-  flavor_name     = "${var.flavor_name}"
-  key_pair        = "${var.key_name}"
-  security_groups = ["${var.security_group_name}"]
-  user_data       = "${local.cloudconfig}"
+  name            = var.environment_name
+  flavor_name     = var.flavor_name
+  key_pair        = var.key_name
+  security_groups = [var.security_group_name]
+  user_data       = local.cloudconfig
 
   # Keep the root disk on a volume
   block_device {
-    uuid             = "${var.block_device_source_id}"
-    source_type      = "${var.block_device_type}"
+    uuid             = var.block_device_source_id
+    source_type      = var.block_device_type
     volume_size      = 30
     boot_index       = 0
     destination_type = "volume"
@@ -32,28 +40,28 @@ resource "openstack_compute_instance_v2" "hub" {
   }
 
   network {
-    name = "${var.network_name}"
+    name = var.network_name
   }
 }
 
 # Determine the volume UUIDs, whether if existing ones were supplied
 # or if new ones were created.
 locals {
-  vol_id_1 = "${length(var.existing_volumes) == 0 ?
-    element(concat(openstack_blockstorage_volume_v2.data.*.id, list("")), 0) :
-    element(concat(var.existing_volumes, list("")), 0)
-  }"
+  vol_id_1 = length(var.existing_volumes) == 0 ? element(
+    concat(openstack_blockstorage_volume_v2.data.*.id, [""]),
+    0,
+  ) : element(concat(var.existing_volumes, [""]), 0)
 }
 
 resource "openstack_blockstorage_volume_v2" "data" {
-  count = "${length(var.existing_volumes) == 0 ? 1 : 0}"
-  name  = "${format("%s-data-%02d", var.environment_name, count.index+1)}"
-  size  = "${var.vol_data_size}"
+  count = length(var.existing_volumes) == 0 ? 1 : 0
+  name  = format("%s-data-%02d", var.environment_name, count.index+1)
+  size  = var.vol_data_size
 }
 
 resource "openstack_compute_volume_attach_v2" "data_1" {
-  instance_id = "${openstack_compute_instance_v2.hub.id}"
-  volume_id   = "${local.vol_id_1}"
+  instance_id = openstack_compute_instance_v2.hub.id
+  volume_id   = local.vol_id_1
 }
 
 resource "ansible_group" "stats" {
@@ -67,8 +75,12 @@ resource "ansible_host" "stats" {
   vars {
     ansible_user = "ptty2u"
 
-    ansible_host            = "${openstack_networking_floatingip_v2.fip.address}"
+    ansible_host            = openstack_networking_floatingip_v2.fip.address
     ansible_ssh_common_args = "-C -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-    syzygy_data_id       = "/dev/disk/by-id/virtio-${substr(openstack_compute_volume_attach_v2.data_1.volume_id, 0, 20)}"
+    syzygy_data_id = "/dev/disk/by-id/virtio-${substr(
+      openstack_compute_volume_attach_v2.data_1.volume_id,
+      0,
+      20,
+    )}"
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,15 @@
 output "instance_uuid" {
-  value = "${openstack_compute_instance_v2.hub.id}"
+  value = openstack_compute_instance_v2.hub.id
 }
 
 output "floating_ip" {
-  value = "${openstack_networking_floatingip_v2.fip.address}"
+  value = openstack_networking_floatingip_v2.fip.address
 }
 
 output "data_id" {
-  value = "/dev/disk/by-id/virtio-${substr(openstack_compute_volume_attach_v2.data_1.volume_id, 0, 20)}"
+  value = "/dev/disk/by-id/virtio-${substr(
+    openstack_compute_volume_attach_v2.data_1.volume_id,
+    0,
+    20,
+  )}"
 }
